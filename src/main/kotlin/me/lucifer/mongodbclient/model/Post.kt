e5ee97365a5ae9d0f012172d3f8f992c6571e0bb
sokkun <sokkun.norn2019@gmail.com>
2023-02-06 22:12:41 +0700

    feature(application): add config mongodb uri

--- src/main/kotlin/me/lucifer/mongodbclient/model/Post.kt
+++ src/main/kotlin/me/lucifer/mongodbclient/model/Post.kt
@@ -1,13 +1,15 @@
 package me.lucifer.mongodbclient.model
 
+import org.jetbrains.annotations.NotNull
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.data.mongodb.core.mapping.Field
 import java.time.LocalTime
 
 @Document(collection = "posts")
 data class Post(
     @Id
-    val id: String? = null,
+    val id: String = "",
 
     var title: String,
 
